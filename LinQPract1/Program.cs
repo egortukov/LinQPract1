d450aed68e717d2a285cd8f7423f9bf4b5fbdd91
egortukov <egor1305991@gmail.com>
2025-01-24 15:32:38 +0900

    1

--- LinQPract1/Program.cs
+++ LinQPract1/Program.cs
@@ -1,9 +1,50 @@
-﻿namespace LinQPract1;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-class Program
+
+List<string> names = new()
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+    "Миша",
+    "Вася",
+    "Петя",
+    "Гриша"
+};
+
+var firstPeson = names.First();
+Console.WriteLine($"первое имя: {firstPeson}");
+
+var secondAndThird = names.Skip(1).Take(2);
+Console.WriteLine("Второе и третье имена:");
+foreach (var name in secondAndThird)
+{
+    Console.WriteLine(name);
+}
+
+var startsWithM = names.Where(names => names.StartsWith("М"));
+Console.WriteLine("Имя которое начинается с буквы 'М'");
+foreach (var name in startsWithM)
+{
+    Console.WriteLine(name);
+}
+
+
+var containsYa = names.Where(name => name.Contains("я"));
+Console.WriteLine("Имена, содержащие букву 'я':");
+foreach (var name in containsYa)
+{
+    Console.WriteLine(name);
+}
+
+var countContainsYa = containsYa.Count();
+Console.WriteLine($"количество людей с буквой 'я' в имени: {countContainsYa}");
+
+var checkForPetya = names.Contains("Петя");
+Console.WriteLine($"проверка на наличие Пети... Петя под номером: {countContainsYa}");
+
+var sortedNames = names.OrderBy(name => name);
+Console.WriteLine("все имена в алфавитном порядке:");
+foreach (var name in sortedNames)
+{
+    Console.WriteLine(name);
 }
